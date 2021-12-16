4309ea7384bb42b032ecf6890caaa5c713a81fcd
Jonathan Franco <jonas150397@gmail.com>
2021-12-16 10:25:45 -0500

    add useMemo

--- src/components/hero/HeroList.jsx
+++ src/components/hero/HeroList.jsx
@@ -1,14 +1,14 @@
+import { useMemo } from "react";
 import { getHeroByPublisher } from "../../selectors/getHeroByPublisher";
+import HeroCard from "./HeroCard";
 
 const HeroList = ({ publisher }) => {
-  const heroes = getHeroByPublisher(publisher);
+  const heroes = useMemo(() => getHeroByPublisher(publisher), [publisher]);
   return (
-    <div>
-      <ul>
-        {heroes.map((heroe) => (
-          <li key={heroe.id}>{heroe.superhero}</li>
-        ))}
-      </ul>
+    <div className="row rows-cols-1 row-cols-md-3 g-3">
+      {heroes.map((hero) => (
+        <HeroCard key={hero.id} {...hero} />
+      ))}
     </div>
   );
 };
